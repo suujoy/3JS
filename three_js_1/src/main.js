d56315f6338cbe4d3cd823f72cdfd573938c56da
Sujoy Saha <ace.sujoy@gmail.com>
2026-03-26 22:01:42 +0530

    three js

---
+++ three_js_1/src/main.js
@@ -0,0 +1,36 @@
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000,
+);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+const canvas = document.querySelector("canvas");
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+window.addEventListener("resize", () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+});
+
+function animate(time) {
+    window.requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    cube.rotation.x = time / 2000;
+    cube.rotation.y = time / 1000;
+}
+
+animate();
